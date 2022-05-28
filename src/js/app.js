9995fa0005a041bb3ea9bffe7c3a2288761e3876
Islam <islamparchev006@gmail.com>
2022-05-28 09:23:57 +0300

    1st commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFuctions from "./modules/functions.js";
+
+flsFuctions.isWebp();
+
+import Swiper, {Navigation, Pagination} from "swiper";
+
+const swiper = new Swiper();
+
+console.log("hello");
